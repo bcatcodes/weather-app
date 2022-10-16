a7c4f077f3b4ffb9f61fc059221445961aab1825
Bcatgray <115519503+bcatcodes@users.noreply.github.com>
2022-10-16 14:43:47 -0500

    adding queryselectors

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+let apiKey = "d1597af0dfaa647ce0306582042cac0e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Houston&appid=${apiKey}&units=imperial`;
 
+axios.get(apiUrl).then(displayTemperature);
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("wind");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.main.wind.speed);
+}
